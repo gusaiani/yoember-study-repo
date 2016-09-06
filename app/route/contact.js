eac18544e2c0aef0513ec975f7623857879e5b36
Gustavo Saiani <gustavo@poe.ma>
2016-09-06 12:13:40 -0300

    Initial commit

---
+++ app/route/contact.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('contact');
+  },
+
+  actions: {
+
+    sendMessage(newContactMessage) {
+      newContactMessage.save().then(() => this.controller.set('responseMessage', true));
+    },
+
+    willTransition() {
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+
+      this.controller.set('responseMessage', false);
+    }
+  }
+});
